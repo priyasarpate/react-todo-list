bd1b705f04e77b84d7a785c63e8a041d8e4f0652
Gunpriya Sarpate <sarpatepriya@gmail.com>
2021-04-11 13:44:44 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import TodoList from './components/TodoList';
+import './components/Todo.css';
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ToDo-App">
+      <TodoList />
+    {/* <ToDoForm /> */}
     </div>
   );
 }
 
 export default App;
+
